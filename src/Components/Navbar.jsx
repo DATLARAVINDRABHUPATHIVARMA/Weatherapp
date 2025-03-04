463d40fc899bb975ef98e1a6129f2482676aac05
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-03-04 09:11:01 +0530

    m-icons

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FilterDramaIcon from '@mui/icons-material/FilterDrama';
+
+function Navbar() {
+  return (
+    <div>
+      <FilterDramaIcon/>
+    </div>
+  )
+}
+
+export default Navbar
