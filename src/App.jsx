627d3eb13bce40c77dd5cef50cd4b0e649a329b6
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-03-05 19:59:53 +0530

    API

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Navbar from './Components/Navbar'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
-  const [weatherData,setWeatherData] = useState(null);
-  const [city,setcity]=useState('London');
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setcity] = useState("London");
 
-  useEffect()
+  useEffect(() => {
+    fetchweatherData(city);
+  }, [city]);
 
   const fetchweatherData = () => {
-    const API_KEY = 'ed88bdc6e4ff9c9381b25488233135b8'
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
-    .then(response=>response.json())
-    .then(data=>{setWeatherData(data);
-      console.log(JSON.stringify(data));
+    const API_KEY = "ed88bdc6e4ff9c9381b25488233135b8";
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
+        console.log(JSON.stringify(data));
+      });
+  };
 
-    });
-  }
+  const handleSearch = (searchedCity) => {
+    setcity(searchedCity);
+  };
 
   return (
     <>
-    <Navbar/>
-     </>
-  )
+      <Navbar onSearch={handleSearch}/>
+    </>
+  );
 }
 
-export default App
+export default App;
