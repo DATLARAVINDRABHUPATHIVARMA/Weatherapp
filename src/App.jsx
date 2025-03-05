79950d10aa422ba614e21e6ffe7a8e48dbca30d0
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2025-03-05 14:22:34 +0530

    API open weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 
 function App() {
+  const [weatherData,setWeatherData] = useState(null);
+  const [city,setcity]=useState('London');
+
+  useEffect()
+
+  const fetchweatherData = () => {
+    const API_KEY = 'ed88bdc6e4ff9c9381b25488233135b8'
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+    .then(response=>response.json())
+    .then(data=>{setWeatherData(data);
+      console.log(JSON.stringify(data));
+
+    });
+  }
 
   return (
     <>
     <Navbar/>
-    </>
+     </>
   )
 }
 
